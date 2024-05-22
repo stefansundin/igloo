7ade6f13225a913a7ab1c1151d638c7c4bf98329
Stefan Sundin <git@stefansundin.com>
2024-05-21 23:33:44 -0700

    Use `ETag` and `If-None-Match` to avoid downloading the certificate if it hasn't changed.

--- src/utils.rs
+++ src/utils.rs
@@ -6,14 +6,19 @@ use rustls::{
 use std::{
   env,
   error::Error,
-  fs, io,
-  io::{BufReader, Cursor, Read, Seek},
-  sync::Arc,
+  fs,
+  io::{self, BufReader, Cursor, Read, Seek},
+  sync::{Arc, OnceLock, RwLock},
 };
 use tokio_rustls::TlsAcceptor;
 use x509_parser::prelude::FromDer;
 use x509_parser::{certificate::X509Certificate, extensions::GeneralName};
 use zip::ZipArchive;
+
+fn etag() -> &'static RwLock<Option<String>> {
+  static ETAG: OnceLock<RwLock<Option<String>>> = OnceLock::new();
+  ETAG.get_or_init(|| RwLock::new(None))
+}
 
 fn extract_cert_and_key<R: Read + Seek>(
   zip_reader: R,
@@ -57,15 +62,36 @@ pub async fn get_tls_acceptor() -> Result<TlsAcceptor, Box<dyn Error + Send + Sy
 
   if let Ok(certificate_url) = env::var("CERTIFICATE_URL") {
     println!("Downloading certificate bundle from {}", certificate_url);
-    let response = reqwest::get(certificate_url).await?;
-    if response.status() != StatusCode::OK {
-      return Err(format!("Bad status code received: {}", response.status()).into());
+    let client = reqwest::Client::new();
+    let mut request = client.get(certificate_url);
+
+    {
+      let etag_option = etag().read().unwrap();
+      if etag_option.is_some() {
+        let etag_value = etag_option.as_ref().unwrap().clone();
+        request = request.header("If-None-Match", etag_value);
+      }
     }
+
+    let response = request.send().await?;
+    if response.status() != StatusCode::OK {
+      return Err(format!("Status code received: {}", response.status()).into());
+    }
+    let response_etag = response
+      .headers()
+      .get("ETag")
+      .and_then(|v| v.to_str().ok().and_then(|v| Some(v.to_string())));
+
     let response_body = response.bytes().await?;
     let cursor = Cursor::new(response_body);
     let data = extract_cert_and_key(cursor)?;
     certs = data.0;
     key = data.1;
+
+    if response_etag.is_some() {
+      let mut etag_option = etag().write().unwrap();
+      *etag_option = response_etag;
+    }
   } else {
     let certificate_path = env::var("CERTIFICATE_PATH")?;
     let certificate_key_path = env::var("CERTIFICATE_KEY_PATH")?;
