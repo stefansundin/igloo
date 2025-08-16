b4a417220da94f7d7b983481595495136059ba1c
Stefan Sundin <git@stefansundin.com>
2025-08-16 13:01:40 -0700

    Simplify ParseS3UrlError.

--- src/s3_url.rs
+++ src/s3_url.rs
@@ -5,12 +5,12 @@ use std::fmt::{Display, Formatter};
 
 pub fn parse(s: &str) -> Result<(String, String), ParseS3UrlError> {
   if !s.starts_with("s3://") {
-    return Err(ParseS3UrlError::new(ParseS3UrlErrorKind::InvalidFormat));
+    return Err(ParseS3UrlError::InvalidFormat);
   }
   let slash_index = s[5..].find('/').map(|i| i + 5);
   if slash_index.is_none() || slash_index == Some(6) {
     // Missing slash (s3://foo) or missing bucket name (s3:///key)
-    return Err(ParseS3UrlError::new(ParseS3UrlErrorKind::InvalidFormat));
+    return Err(ParseS3UrlError::InvalidFormat);
   }
   let slash_index = slash_index.unwrap();
   let bucket = s[5..slash_index].to_string();
@@ -18,26 +18,17 @@ pub fn parse(s: &str) -> Result<(String, String), ParseS3UrlError> {
   Ok((bucket, key))
 }
 
-#[derive(Debug)]
-pub struct ParseS3UrlError {
-  kind: ParseS3UrlErrorKind,
-}
-
-impl ParseS3UrlError {
-  fn new(kind: ParseS3UrlErrorKind) -> Self {
-    Self { kind }
-  }
+#[derive(Debug, PartialEq)]
+pub enum ParseS3UrlError {
+  InvalidFormat,
 }
 
 impl Display for ParseS3UrlError {
   fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
-    write!(f, "Error parsing S3 URL: {:?}", self.kind)
+    match self {
+      ParseS3UrlError::InvalidFormat => write!(f, "Error parsing S3 URL"),
+    }
   }
 }
 
 impl Error for ParseS3UrlError {}
-
-#[derive(Debug, PartialEq)]
-enum ParseS3UrlErrorKind {
-  InvalidFormat,
-}
